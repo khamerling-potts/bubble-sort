38c5f430028ab240e2f7423b8bc7deee6c76cd4f
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-11-16 13:09:58 -0500

    done

--- javascript/bubble_sort.js
+++ javascript/bubble_sort.js
@@ -1,5 +1,17 @@
 function bubbleSort(arr) {
-  // type your code here
+  debugger;
+  let swaps = 0;
+  for (let i = 0; i < arr.length - 1; i++) {
+    const next = arr[i + 1];
+    const element = arr[i];
+    if (next < element) {
+      arr[i] = next;
+      arr[i + 1] = element;
+      swaps++;
+    }
+  }
+  if (swaps === 0) return arr;
+  return bubbleSort(arr);
 }
 
 if (require.main === module) {
@@ -21,9 +33,23 @@ if (require.main === module) {
 
   console.log("Expecting: [1, 2, 3]");
   console.log("=>", bubbleSort([2, 3, 1]));
+
+  console.log("expecting [5, 7, 9, 12, 15, 17]");
+  console.log(bubbleSort([15, 7, 5, 17, 12, 9]));
 }
 
 module.exports = bubbleSort;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+/* 
+for each element, index in array
+  next element = array[i+1]
+  if next element is less than element
+    swap using temp variables
+    swaps plus 1
+  if swaps is zero, return array
+  otherwise, run function again
+
+  O(n^2)
+*/
